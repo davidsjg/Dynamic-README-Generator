cf1996d5eb1d28f86c2030348c1b0d441d7e23f0
Joey Davidson <j.davidson18@gmail.com>
2021-02-14 10:05:13 -0700

    finished render license function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
+const path = require("path")
+
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = []
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+//                pathing to certain file.  cwd is current working directory, so we're pathing into our cwd.  fileName is what we pass into the function as well as the data
+    fs.writeFileSync(path.join(process.cwd(), fileName),data)
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "githubname",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the project’s title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a short description of your project:"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of license should your project have?",
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        },
+        //data is all of the answers from above responses
+    ]).then (data => {
+        writeToFile("README.md", generateMarkdown(data))
+
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Dynamic-README-Generator
-Application to generate README based on user inputs
+# Readme
+
+${renderLicenseBadge(data.license)}
+
+${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+${renderLicenseLink(data.license)}
+
+- [Contributing](#contributing)
+
+- [Test](#tests)
+
+- [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+${installation}
+```
+
+## Usage
+
+${usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${contributing}
+
+## Tests
+
+${tests}
+
+## Questions
+
+${questions}
